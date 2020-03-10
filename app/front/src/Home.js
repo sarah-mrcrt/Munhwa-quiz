2057c8ad7474aa123339072c2053f976ae805646
unknown <agathedekerle@yahoo.fr>
2020-03-10 13:26:53 +0100

    aoziriazr

--- app/front/src/Home.js
+++ app/front/src/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React,  { useState },  { useEffect }, {Component} from "react";
 import logo from './logo.svg';
 // import './App.css';
 import QuizzThumbnail from "./Quizz/QuizzThumbnail";
@@ -6,11 +6,24 @@ import QuizzThumbnail from "./Quizz/QuizzThumbnail";
 // import About from "./About.js";
 // import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 
+
 function Home() {
+     let [quizzes, setQuizzes ] = useState([]);
+    
+    async function getQuizzes() {  // The function is asynchronous
+                const q = (await axios.get('http://localhost:8000/quizzes')).data; 
+                setQuizzes(q);
+            }
+
+     useEffect(() => { // this is a hook called everytime the function is rendered again  // import useEffect
+             getQuizzes()
+    }, []);
+
+
+
   return (
     <div className="Home">
-        {/* <Route exact={true} path="/quote/:id" component={Quizz}/> */}
-        { /*<Route exact={true} path="/quote/:id" component={QuizzThumbnail}/> */}
+       <QuizzThumbnail quiz={q} />
     </div>
   );
 }
--- app/front/src/Quizz/QuizzThumbnail.js
+++ app/front/src/Quizz/QuizzThumbnail.js
@@ -1,33 +1,13 @@
 import React, {Component} from "react";
-//Lancer le quiz.
+//Lvoir les quizzs
 
-function QuizzThumbnail(quizzes) {
- async function addQuizz(e){
-        e.preventDefault();
-        let q = {
-//            id : idx++,
-//            name : e.target.elements[0].value,
-//            picture_url : e.target.elements[1].value,
-//            keywords : e.target.elements[2].value,
-//            users_id : e.target.elements[3].value
-        }
-        insertQuizz(q);
-    }
-    
-let jsxQuizzes =  quizzes
-      .map(q=>
-        <QuizzLOL  
-              name={q.name}
-              picture_url={q.picture_url}
-              keywords={q.keywords}
-              users_id={q.users_id}
-              deleteQuizz={deleteQuizz}/>);
+function QuizzThumbnail(props) {
+     
 
-    
   return (
     <div className="QuizzThumbnail">
-      QuizzThumbnail
-      {quizzes.name}
+      
+      {props.quiz.name}
     </div>
   );
 }
