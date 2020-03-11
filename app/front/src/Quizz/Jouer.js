079c190e4dfc992f331b983eddb1ce4e5f8d11a5
unknown <agathedekerle@yahoo.fr>
2020-03-11 15:17:55 +0100

    j'suis fiere de jouer

--- app/front/src/Quizz/Jouer.js
+++ app/front/src/Quizz/Jouer.js
@@ -8,6 +8,7 @@ import { HTTP_SERVER_PORT, HTTP_SERVER_PORT_PICTURES,HTTP_SERVER_PORT_VIDEOS} fr
  function Jouer(props) {
       let [quizz, setQuizz ] = useState(null);
       let [questions, setQuestions ] = useState([]);
+      let [answers, setAnswers ] = useState([]);
       const [current, setCurrent] = useState(0);
 
      async function getQuizz() {  // The function is asynchronous
@@ -18,19 +19,28 @@ import { HTTP_SERVER_PORT, HTTP_SERVER_PORT_PICTURES,HTTP_SERVER_PORT_VIDEOS} fr
                  const qt = (await axios.get(HTTP_SERVER_PORT +'questions/'+props.match.params.id)).data;
                  setQuestions(qt);
              }
-
+     async function getAnswers() {
+                  const aw = (await axios.get(HTTP_SERVER_PORT +'answers/'+props.match.params.id)).data;
+                  setAnswers(aw);
+     }
       useEffect(() => {
               getQuizz();
               getQuestions();
+              getAnswers();
      }, []);
 
 function suivant(e) {
     e.preventDefault();
     setCurrent(current+1);
 }
+function answer(e) {
+    e.preventDefault();
+
+
+}
 console.log("zz", props);
 
-if(questions.length == 0){
+if(questions.length == null){
     return 'En cours de oute'
     }
     if(current >= questions.length)
@@ -39,12 +49,21 @@ if(questions.length == 0){
     )
    return (
      <div className="Home">
-      Bonjour je suis les questions {props.match.params.id}
-      <br/>BONJOUR J'AIMERAIS AFFICHER LES QUESTIONS!!!!!!!
+      Bonjour je suis les questions
+      <br/> Courage mes petites CSS :*
         <p>{questions[current].sentence} </p>
-        </div>
-        
+        // <li onClick={(e) => answer(e)}>{answers.sentence}</li>
+         <li> {answers.sentence } {console.log(answers)} </li>
+         {answers.map((item, i) => {
+           return(
+             <li>{item.sentence}</li>
+           )
+           
+         })
+         }
         <button onClick={e => suivant(e)}>Next</button>
+        </div>
+
 
    );
  }
